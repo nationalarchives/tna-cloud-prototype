35cd6b4e9c0ce776dee7358e57409b341f0afb45
Chris Bishop <domingobishop@gmail.com>
2017-03-24 16:37:13 +0000

    first commit

---
+++ tna-cloud.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: TNA Cloud
+ * Plugin URI: https://github.com/nationalarchives/tna-cloud-prototype
+ * Description: The National Archives Cloud plugin
+ * Version: 0.1
+ * Author: The National Archives
+ * Author URI: https://github.com/nationalarchives
+ * License: GPL2
+ */
+
